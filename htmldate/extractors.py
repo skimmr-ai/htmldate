6de40d180674a4391afcf72da58bcbead515fca6
Pavel Andrienko <pasha.ne@gmail.com>
2026-03-02 09:48:13 +0200

    Increase MAX_SEGMENT_LEN from 52 to 54

--- htmldate/extractors.py
+++ htmldate/extractors.py
@@ -83,7 +83,7 @@ DATE_EXPRESSIONS = """
 
 FREE_TEXT_EXPRESSIONS = XPath(FAST_PREPEND + "/text()")
 MIN_SEGMENT_LEN = 6
-MAX_SEGMENT_LEN = 52
+MAX_SEGMENT_LEN = 54
 
 # discard parts of the webpage
 # archive.org banner inserts
